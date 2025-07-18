a9ee3110070ac8bdcf0d4ee0f5cec2f78048aadb
codebyemma <mysubbacc@gmail.com>
2025-07-18 16:04:45 +0100

    now

--- alx-react-app-props/src/components/ProfilePage.jsx
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -1,6 +1,9 @@
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 import UserInfo from './UserInfo';
 
 function ProfilePage() {
+	 const userData = useContext(UserContext);
   return <UserInfo />;
 }
 
