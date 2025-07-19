19ba129e206e9d76a67b1904a5af73a88df87e7b
codebyemma <mysubbacc@gmail.com>
2025-07-19 18:47:26 +0100

    done

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
 	return (
-		<nav>
+		<nav style={{backgroundColor: 'white', display: 'flex', justifyContent: 'center'}}>
 		<Link to="/">Home</Link> | {"  "}
 		<Link to="/about">About</Link> | {"  "}
 		<Link to="/services">Services</Link>  | {"  "}
