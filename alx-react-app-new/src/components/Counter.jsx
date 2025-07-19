d6ce8d020b286b1c5377bf7075d6ac6c65cf7d50
codebyemma <mysubbacc@gmail.com>
2025-07-19 18:54:31 +0100

    dome

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -1,22 +1,14 @@
 import { useState } from 'react';
 
 function Counter() {
-	const [ count, setCount ] = useState(0);
-	const increase = () => setCount(count + 1);
-
-	const decrease = () => setCount(count - 1);
-
-	const reset = () => setCount(0);
+	const [count, setCount] = useState(0);
 
 	return (
-	<div style={{display: 'flex',
-	gap: '30px'}}>
-	<p>Current Count: {count}</p>
-	<button onClick={increase}>Increament</button>
-	<button onClick={decrease}>Decreament</button>
-	<button onClick={reset}>Reset</button>
-	</div>
+		<div>
+		<p>Current Count: {count}</p>
+		<button onClick={() => setCount(count + 1)}>Increment</button>
+		<button onClick={() => setCount(count - 1)}>Decrement</button>
+		<button onClick={() => setCount(0)}>Reset</button>
+		</div>
 	);
 }
-
-export default Counter;
